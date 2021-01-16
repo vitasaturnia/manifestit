dc15021b4d0f4bfcb3f5b1d8673f57cd0d435f6d
TONY TONY <tonyelpatron@icloud.com>
2021-01-16 15:08:37 +0100

    patch

--- src/pages/Hardware.js
+++ src/pages/Hardware.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import { FaLaptop }  from 'react-icons/fa';
+import { FaLaptop, FaMobileAlt, FaNetworkWired, FaWifi, FaCode }  from 'react-icons/fa';
+
 
 
 const Hardware = () => (
@@ -20,13 +21,18 @@ Hardware en tech support
         <FaLaptop />
   <h3>Computer reparatie</h3>
   <p>Wij repararen laptops en computers van alle merken, wij kunnen u ook helpen uw zelf samengestelde PC te monteren of uw huidige PC of laptop te upgraden</p>
-
+  <FaMobileAlt />
   <h2>iPhone reparatie</h2>
   <p>Wij repareren alle iPhones tegen zeer scherpe prijzen, voor de nieuwste iphones hebben wij ook altijd onderdelen op voorraad, dus hoeft u nooit lang te wachten!</p>
+  <FaWifi />
   <h2>Aansluiten van netwerken</h2>
   <p>Wilt u een netwerk aansluiten bij u bedrijf, of wilt u thuis een netwerk aansluiten? Heeft u problemen met uw huidige netwerk, of wilt u uw huidige netwerk verbeteren?</p>
+  <FaNetworkWired />
   <h2>Aansluiten van IOT apparaten</h2>
+
   <p>Automatische lichten, koffie op schakelaar, beveiligingscameras, video deurbel, of andere smart home apparaten? Geen probleem, wij sluiten ze voor u aan!</p>
+  <FaCode />
+
   <h2>Troubleshooting</h2>
   <p>Heeft u een probleem maar weet u niet wat? Laat een van onze experts een handje helpen. </p>
   </div>
