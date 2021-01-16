ead826ff2a239e3aeac31f14c13cb00e957cfe28
TONY TONY <tonyelpatron@icloud.com>
2021-01-16 12:59:59 +0100

    patch

--- src/pages/Hardware.js
+++ src/pages/Hardware.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import { FaLaptop } from 'react-icons';
+import FaLaptop  from 'react-icons/fa';
 
 
 const Hardware = () => (
