afb36661a234e7f0773120f1611d88f2f7cddcca
TONY TONY <tonyelpatron@icloud.com>
2021-01-21 22:06:54 +0100

    patch

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -25,18 +25,7 @@ export const BlogPostTemplate = ({
            
             <p>{description}</p>
             <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map((tag) => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
+          
           </div>
         </div>
       </div>
